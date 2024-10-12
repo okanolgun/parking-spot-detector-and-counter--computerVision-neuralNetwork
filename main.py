5bd65203c255a598ea8006997a7d351c8a008bec
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-10-12 15:25:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import cv2
-from util import get_parking_spots_bboxes
 
-mask = './mask_crop.png'
 video_path = './data/parking_crop_loop.mp4'
 
-mask = cv2.imread(mask, 0)
-
 cap = cv2.VideoCapture(video_path)
-
-connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
-
-spots = get_parking_spots_bboxes(connected_components)
-
-print(spots[0])
 
 ret = True
 while ret:
@@ -24,3 +14,4 @@ while ret:
 
 cap.release()
 cv2.destroyAllWindows()
+
