d898ae4d8654ac6414d3628d683ebe66aff866b1
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-10-14 17:38:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+
+from util import get_parking_spots_bboxes, empty_or_not
+
+mask = './mask_1920_1080.png'
+video_path = './data/parking_1920_1080_loop.mp4'
+
+mask = cv2.imread(mask, 0)
+
+cap = cv2.VideoCapture(video_path)
+
+connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+
+spots = get_parking_spots_bboxes(connected_components)
+
+def calc_diff(im1, im2):
+    return np.mean(im1) - np.mean(im2)
+
+# print(spots[0])
+# empty coordinates for first run
+
+step = 30
+spots_status = [None for j in spots]
+diffs = [Node for j in spots]
+
+frame_nmr = 0
+ret = True
+while ret:
+    ret, frame = cap.read()
+
+    if frame_nmr % step == 0:
+        for spot_indx, spot in enumerate(spots):
+            x1, y1, w, h = spot
+
+            spot_crop = frame[y1:y1+h, x1:x1+w, :]
+
+            spot_status = empty_or_not(spot_crop)
+
+            spots_status[spot_indx] = spot_status
+
+    for spot_indx, spot in enumerate(spots):
+        spot_status = spots_status[spot_indx]
+        x1, y1, w, h = spots[spot_indx]
+
+        if spot_status:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+        else:
+            frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
+
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+    frame_nmr = frame_nmr + 1
+
+cap.release()
+cv2.destroyAllWindows()
