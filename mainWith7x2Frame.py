835f702d9685457bf16859dbaef967c41c742f41
Okan Olgun <98118524+okanolgun@users.noreply.github.com>
2024-10-21 16:31:50 +0300

    Update mainWith7x2Frame.py

--- mainWith7x2Frame.py
+++ mainWith7x2Frame.py
@@ -8,6 +8,7 @@ mask = cv2.imread(mask, 0)
 # mask.png and video.mp4 is already in our data set and our project directory. 
 
 cap = cv2.VideoCapture(video_path)
+# we read our mask png and turned it to a numpy array
 
 connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
 
